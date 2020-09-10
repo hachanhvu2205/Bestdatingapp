b262266ca87cc5d886ac0eea80d348bbb41fe386
unknown <bitinhi3246@gmail.com>
2020-09-10 19:15:03 +0700

    change home

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SwipePage(),
+      home: LogInPage(),
     );
   }
 }
